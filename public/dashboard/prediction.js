f56f5ad76149b98a3e239ee99f14a844e7eef36b
Kumail Khan <hassankumail9@gmail.com>
2021-05-11 21:59:41 +0500

    added validation

--- public/dashboard/prediction.js
+++ public/dashboard/prediction.js
@@ -3,12 +3,24 @@
 document.getElementById('predictBtn').addEventListener('click', async () => {
     // document.getElementById("headline").innerText = "Form is working!";
 
+
     let temperature = parseFloat(document.getElementById("temperature").value),
         turbidity = parseFloat(document.getElementById("turbidity").value),
         ph = parseFloat(document.getElementById("ph").value),
         dsolids = parseFloat(document.getElementById("dsolids").value),
-        doxygen = parseFloat(document.getElementById("doxygen").value)
+        doxygen = parseFloat(document.getElementById("doxygen").value);
 
+
+    // Validate Inputs
+    let validation = validateInput(temperature,
+        turbidity,
+        ph,
+        dsolids,
+        doxygen);
+
+    if (!validation) {
+        return;
+    }
     let data = {
         temperature,
         turbidity,
@@ -31,7 +43,7 @@ document.getElementById('predictBtn').addEventListener('click', async () => {
         body: JSON.stringify(data)
     });
 
-    
+
     result = await result.json();
 
     document.getElementById('model-output').textContent = "The Water Quality is " + result.result;
@@ -39,3 +51,24 @@ document.getElementById('predictBtn').addEventListener('click', async () => {
 
 });
 
+
+function validateInput(temperature,turbidity,ph,dsolids,doxygen) {
+
+    if (isNaN(temperature) || isNaN(turbidity) || isNaN(ph) || isNaN(dsolids) || isNaN(doxygen)) {
+        document.getElementById('model-output').textContent = "Value of WQPs can't be empty.";
+        return;
+    }
+    let tempMsg = document.getElementById('temp-val'),
+        turbidityMsg = document.getElementById('turbidity-val'),
+        phMsg = document.getElementById('ph-val'),
+        dsolidsMsg = document.getElementById('dsolids-val'),
+        doxygenMsg = document.getElementById('doxygen-val');
+
+    if (!(0 < temperature < 50)) {
+        tempMsg.innerText = "Temperature value should be in range 0 to 50"
+    }
+
+    if (!(0 < temperature < 50)) {
+        turbidityMsg.innerText = "Temperature value should be in range 0 to 50"
+    }
+}
